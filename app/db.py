1792792d25f6daaf29cee236477522837423ee24
Pommesdieb <pommesdieb@googlemail.com>
2025-10-04 02:20:03 +0200

    Create db.py

---
+++ app/db.py
@@ -0,0 +1,14 @@
+from contextlib import contextmanager
+from sqlmodel import SQLModel, Session, create_engine
+
+# SQLite-Datei im Projektroot
+engine = create_engine("sqlite:///./ordo.db", connect_args={"check_same_thread": False})
+
+
+def init_db() -> None:
+    SQLModel.metadata.create_all(engine)
+
+
+def get_session():
+    with Session(engine) as session:
+        yield session
